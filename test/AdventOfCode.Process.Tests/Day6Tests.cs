5b66aee7bfb5b55867fb6549765a4133e62f2ad9
Patrick Handberg Jessen <119584261+PatrickHJessen@users.noreply.github.com>
2023-12-06 12:20:11 +0100

    Added test for day 6

---
+++ test/AdventOfCode.Process.Tests/Day6Tests.cs
@@ -0,0 +1,43 @@
+namespace AdventOfCode.Process.Tests;
+
+public class Day6Tests
+{
+    private readonly IDataAccess _dataAccess;
+    private readonly IDay _day;
+    private readonly int _dayNumber;
+
+    public Day6Tests()
+    {
+        _dataAccess = new DataAccess();
+        _day = new Day6();
+        _dayNumber = 6;
+    }
+
+    [Fact]
+    public void PartATests()
+    {
+        // Arrange
+        string[] data = _dataAccess.GetData($"../../../../../src/AdventOfCode.Data/data/test/{_dayNumber}.txt");
+        string expected = "288";
+
+        // Act
+        string actual = _day.PartA(data);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void PartBTests()
+    {
+        // Arrange
+        string[] data = _dataAccess.GetData($"../../../../../src/AdventOfCode.Data/data/test/{_dayNumber}.txt");
+        string expected = "Not finished!";
+
+        // Act
+        string actual = _day.PartB(data);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+}
