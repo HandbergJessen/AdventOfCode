da00cbab622592a3e63d76bca300ecf6934e4519
Brian Handberg Jessen <brianj@stubnet.dk>
2023-12-15 21:27:29 +0100

    Day 14 and 15 completed

--- src/AdventOfCode.Process/Day14.cs
+++ src/AdventOfCode.Process/Day14.cs
@@ -1,14 +1,260 @@
+using System.Runtime.CompilerServices;
+
 namespace AdventOfCode.Process;
 
 public class Day14 : IDay
 {
     public string PartA(string[] input)
     {
-        return "Not finished!";
+        Rock[,] grid = GenerateGrid(input);
+
+        grid = PlatformTiltUp(grid);
+
+        return TotalLoadY(grid).ToString();
     }
 
     public string PartB(string[] input)
     {
-        return "Not finished!";
+        Rock[,] grid = GenerateGrid(input);
+        List<long> totalLoadsY = new();
+        List<long> totalLoadsX = new();
+
+        long totalLoadY = 0;
+        long totalLoadX = 0;
+        bool alreadyExists = false;
+        long endValue = 0;
+        do
+        {
+            if (totalLoadY != 0)
+            {
+                totalLoadsY.Add(totalLoadY);
+                totalLoadsX.Add(totalLoadX);
+
+            }
+            grid = PlatformTiltUp(grid);
+            grid = PlatformTiltLeft(grid);
+            grid = PlatformTiltDown(grid);
+            grid = PlatformTiltRight(grid);
+
+            totalLoadY = TotalLoadY(grid);
+            totalLoadX = TotalLoadX(grid);
+
+            for (int i = 0; i < totalLoadsX.Count; i++)
+            {
+                if (totalLoadsY[i] == totalLoadY && totalLoadsX[i] == totalLoadX)
+                {
+                    endValue = totalLoadsY[(1000000000 - i) % (totalLoadsX.Count - i) + i - 1];
+
+                    alreadyExists = true;
+                    break;
+                }
+            }
+        } while (!alreadyExists);
+
+        return endValue.ToString();
+    }
+
+
+    private static Rock[,] GenerateGrid(string[] input)
+    {
+        Rock[,] grid = new Rock[input[0].Length, input.Length];
+
+        for (int y = 0; y < input.Length; y++)
+        {
+            string row = input[y];
+
+            for (int x = 0; x < row.Length; x++)
+            {
+                char rock = row[x];
+
+                if (rock != '.')
+                {
+                    grid[y, x] = new Rock(x, y, rock);
+                }
+                else
+                {
+                    grid[y, x] = new Rock(x, y, ' ');
+                }
+            }
+        }
+
+        return grid;
+    }
+    private static Rock[,] PlatformTiltUp(Rock[,] grid)
+    {
+
+        for (int y = 0; y < grid.GetLength(0); y++)
+        {
+            for (int x = 0; x < grid.GetLength(1); x++)
+            {
+                if (grid[y, x].Visual == 'O')
+                {
+                    int newY = y;
+                    for (int up = y - 1; up >= 0; up--)
+                    {
+                        if (grid[up, x].Visual == ' ')
+                        {
+                            newY = up;
+                        }
+                        else break;
+                    }
+                    if (newY != y)
+                    {
+                        grid[newY, x].Visual = 'O';
+                        grid[y, x].Visual = ' ';
+                    }
+                }
+            }
+        }
+
+        return grid;
+    }
+    private static Rock[,] PlatformTiltDown(Rock[,] grid)
+    {
+        for (int y = grid.GetLength(0) - 1; y >= 0; y--)
+        {
+            for (int x = 0; x < grid.GetLength(1); x++)
+            {
+                if (grid[y, x].Visual == 'O')
+                {
+                    int newY = y;
+                    for (int down = y + 1; down < grid.GetLength(0); down++)
+                    {
+                        if (grid[down, x].Visual == ' ')
+                        {
+                            newY = down;
+                        }
+                        else break;
+                    }
+                    if (newY != y)
+                    {
+                        grid[newY, x].Visual = 'O';
+                        grid[y, x].Visual = ' ';
+                    }
+                }
+            }
+        }
+
+        return grid;
+    }
+    private static Rock[,] PlatformTiltLeft(Rock[,] grid)
+    {
+
+        for (int y = 0; y < grid.GetLength(0); y++)
+        {
+            for (int x = 0; x < grid.GetLength(1); x++)
+            {
+                if (grid[y, x].Visual == 'O')
+                {
+                    int newX = x;
+                    for (int right = x - 1; right >= 0; right--)
+                    {
+                        if (grid[y, right].Visual == ' ')
+                        {
+                            newX = right;
+                        }
+                        else break;
+                    }
+                    if (newX != x)
+                    {
+                        grid[y, newX].Visual = 'O';
+                        grid[y, x].Visual = ' ';
+                    }
+                }
+            }
+        }
+
+        return grid;
+    }
+    private static Rock[,] PlatformTiltRight(Rock[,] grid)
+    {
+
+        for (int y = 0; y < grid.GetLength(0); y++)
+        {
+            for (int x = grid.GetLength(1) - 1; x >= 0; x--)
+            {
+                if (grid[y, x].Visual == 'O')
+                {
+                    int newX = x;
+
+                    for (int left = x + 1; left < grid.GetLength(1); left++)
+                    {
+                        if (grid[y, left].Visual == ' ')
+                        {
+                            newX = left;
+                        }
+                        else break;
+                    }
+                    if (newX != x)
+                    {
+                        grid[y, newX].Visual = 'O';
+                        grid[y, x].Visual = ' ';
+                    }
+                }
+            }
+        }
+
+        return grid;
+    }
+
+    private class Rock
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public char Visual { get; set; }
+
+
+        public Rock(int x, int y, char visual)
+        {
+            X = x;
+            Y = y;
+            Visual = visual;
+
+        }
+    }
+    private static void Print(Rock[,] grid)
+    {
+        for (int y = 0; y < grid.GetLength(0); y++)
+        {
+            for (int x = 0; x < grid.GetLength(1); x++)
+            {
+                Console.Write(grid[y, x].Visual);
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+
+    }
+    private static long TotalLoadY(Rock[,] grid)
+    {
+        long totalValue = 0;
+
+        for (int y = 0; y < grid.GetLength(0); y++)
+        {
+            for (int x = 0; x < grid.GetLength(1); x++)
+            {
+                if (grid[y, x].Visual == 'O')
+                {
+                    totalValue += grid.GetLength(0) - y;
+                }
+            }
+        }
+        return totalValue;
+    }
+    private static long TotalLoadX(Rock[,] grid)
+    {
+        long totalValue = 0;
+
+        for (int x = 0; x < grid.GetLength(1); x++)
+        {
+            for (int y = 0; y < grid.GetLength(0); y++)
+            {
+                if (grid[y, x].Visual == 'O')
+                {
+                    totalValue += grid.GetLength(1) - x;
+                }
+            }
+        }
+        return totalValue;
     }
 }
--- src/AdventOfCode.Process/Day15.cs
+++ src/AdventOfCode.Process/Day15.cs
@@ -4,11 +4,91 @@ public class Day15 : IDay
 {
     public string PartA(string[] input)
     {
-        return "Not finished!";
+
+        string[] sequences = input[0].Split(',');
+        long focusPower = 0;
+        foreach (string sequence in sequences)
+        {
+            int currentValue = 0;
+            char[] details = sequence.ToCharArray();
+            foreach (char detail in details)
+            {
+                currentValue += detail;
+                currentValue = GetHASHValue(currentValue);
+
+            }
+            focusPower += currentValue;
+        }
+
+        return focusPower.ToString();
     }
 
     public string PartB(string[] input)
     {
-        return "Not finished!";
+        string[] sequences = input[0].Split(',');
+        long focusPower = 0;
+
+        IDictionary<int, List<string>> boxes = new Dictionary<int, List<string>>();
+        IDictionary<string, int> focalLengths = new Dictionary<string, int>();
+        foreach (string sequence in sequences)
+        {
+            string[] lenseDetails = sequence.Split('-', '=');
+
+            _ = int.TryParse(lenseDetails[1], out int focalLenght);
+            string lenseId = lenseDetails[0];
+
+            focalLengths[lenseId] = focalLenght;
+
+            int box = 0;
+            char[] details = lenseDetails[0].ToCharArray();
+            foreach (char detail in details)
+            {
+                box += detail;
+                box = GetHASHValue(box);
+            }
+
+            if (!boxes.ContainsKey(box))
+            {
+                boxes[box] = new();
+            }
+
+            if (RemoveLens(sequence))
+            {
+                boxes[box].Remove(lenseId);
+            }
+            else
+            {
+                if (!boxes[box].Contains(lenseId))
+                {
+                    boxes[box].Add(lenseId);
+                }
+            }
+        }
+
+        foreach (int boxer in boxes.Keys)
+        {
+            List<string> lensIds = boxes[boxer];
+            for (int i = 0; i < lensIds.Count; i++)
+            {
+                int focalLength = focalLengths[lensIds[i]];
+
+                focusPower += (boxer + 1) * focalLength * (i + 1);
+            }
+        }
+
+        return focusPower.ToString();
+    }
+
+    private static bool RemoveLens(string operation)
+    {
+        if (operation.Contains('-')) return true;
+        else return false;
+    }
+    private static int GetHASHValue(int currentValue)
+    {
+        currentValue *= 17;
+        currentValue %= 256;
+
+        return currentValue;
     }
 }
