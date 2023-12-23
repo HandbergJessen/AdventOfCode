cc8a19f16cab1aaed48dfbbb0a8179aee28b3f74
Brian Handberg Jessen <brianj@stubnet.dk>
2023-12-23 13:48:07 +0100

    Day 22 completed OK + Day 21 part 2 (not working)

--- src/AdventOfCode.Process/Day21.cs
+++ src/AdventOfCode.Process/Day21.cs
@@ -6,25 +6,100 @@ public class Day21 : IDay
 {
     public string PartA(string[] input)
     {
-        // 3818 3644
         GardenPlot[,] gardenPlots = GenerateGarden(input);
-        //PrintGarden(gardenPlots);
-        GardenPlot gardenPlot = GetStartPlot(gardenPlots);
 
+        var (start, steps) = GetValues(input.Length, 'A');
+        GardenPlot startPlot = gardenPlots[start, start];
 
+        GetDistances(gardenPlots, startPlot);
+        PrintDistances(gardenPlots, steps);
 
-        GetDistances(gardenPlots, gardenPlot);
-
-        PrintDistances(gardenPlots);
-
-        return CountPlots(gardenPlots).ToString();
+        return CountPlots(gardenPlots, steps).ToString();
     }
 
     public string PartB(string[] input)
     {
-        return "Not finished!";
+        GardenPlot[,] gardenPlots = GenerateGardenB(input);
+        var (start, steps) = GetValues(input.Length, 'B');
+        GardenPlot startPlot = gardenPlots[start, start];
+
+        GetDistances(gardenPlots, startPlot);
+        //PrintDistances(gardenPlots, steps);
+        int[,] blocks = CountPlotsXY(gardenPlots, steps);
+
+        int m = blocks[2, 2];
+        int e = blocks[1, 2];
+        int t = blocks[2, 0] + blocks[0, 2] + blocks[4, 2] + blocks[2, 4];
+        int a = blocks[1, 1] + blocks[1, 3] + blocks[3, 1] + blocks[3, 3];
+        int b = blocks[0, 1] + blocks[0, 3] + blocks[4, 1] + blocks[4, 3];
+        long n = 26501365 / 131;
+        long total = (n - 1) * (n - 1) * m + n * n * e + (n - 1) * a + n * b + t;
+
+        Console.WriteLine(m);
+        Console.WriteLine(e);
+        Console.WriteLine(t);
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+
+        return total.ToString();
     }
 
+    private static int[,] CountPlotsXY(GardenPlot[,] gardenPlots, int steps)
+    {
+        int[,] blocks = new int[5, 5];
+        int blockX;
+        int blockY;
+        for (int y = 0; y < gardenPlots.GetLength(0); y++)
+        {
+            for (int x = 0; x < gardenPlots.GetLength(1); x++)
+            {
+                blockX = x / 131;
+                blockY = y / 131;
+                GardenPlot gardenPlot = gardenPlots[y, x];
+                if (gardenPlot.Visual != '#' && gardenPlot.Distance <= steps && gardenPlot.Distance % 2 == 0)
+                {
+                    blocks[blockY, blockX]++;
+                }
+            }
+        }
+
+        /*for (int y = 0; y < blocks.GetLength(0); y++)
+        {
+            for (int x = 0; x < blocks.GetLength(1); x++)
+            {
+                Console.Write($"  {blocks[y, x]}");
+            }
+            Console.WriteLine();
+        }*/
+
+        return blocks;
+    }
+
+    public static (int, int) GetValues(int length, char part)
+    {
+
+        int start = 0;
+
+        int steps = 0;
+        if (part == 'A')
+        {
+            start = (length - 1) / 2;
+            if (length < 131)
+                steps = 6;
+            else
+                steps = 64;
+        }
+        else
+        {
+            start = ((length - 1) / 2) + 2 * length;
+            if (length < 131)
+                steps = 2 * length + ((length - 1) / 2);
+            else
+                steps = 2 * length + ((length - 1) / 2);
+        }
+
+        return (start, steps);
+    }
     private static GardenPlot GetStartPlot(GardenPlot[,] gardenPlots)
     {
         foreach (GardenPlot gardenPlot in gardenPlots)
@@ -93,6 +168,22 @@ public class Day21 : IDay
         }
         return gardenplots;
     }
+    private static GardenPlot[,] GenerateGardenB(string[] input)
+    {
+
+        GardenPlot[,] gardenplots = new GardenPlot[input.Length * 5, input.Length * 5];
+        for (int y = 0; y < input.Length * 5; y++)
+        {
+            string row = input[y % input.Length];
+            for (int x = 0; x < row.Length * 5; x++)
+            {
+                char plot = row[x % input.Length];
+                gardenplots[y, x] = new GardenPlot(y, x, plot);
+
+            }
+        }
+        return gardenplots;
+    }
     private class GardenPlot
     {
         public int Y { get; private set; }
@@ -124,14 +215,14 @@ public class Day21 : IDay
         }
     }
 
-    private static void PrintDistances(GardenPlot[,] gardenPlots)
+    private static void PrintDistances(GardenPlot[,] gardenPlots, int steps)
     {
 
         for (int y = 0; y < gardenPlots.GetLength(0); y++)
         {
             for (int x = 0; x < gardenPlots.GetLength(1); x++)
             {
-                if (gardenPlots[y, x].Visual == '#' || gardenPlots[y, x].Distance > 64 || gardenPlots[y, x].Distance % 2 != 0)
+                if (gardenPlots[y, x].Visual == '#' || gardenPlots[y, x].Distance > steps || gardenPlots[y, x].Distance % 2 != 0)
                 {
                     Console.Write(gardenPlots[y, x].Visual);
                 }
@@ -145,12 +236,12 @@ public class Day21 : IDay
             Console.WriteLine();
         }
     }
-    private static int CountPlots(GardenPlot[,] gardenPlots)
+    private static int CountPlots(GardenPlot[,] gardenPlots, int steps)
     {
         int countPlots = 0;
         foreach (GardenPlot gardenPlot in gardenPlots)
         {
-            if (gardenPlot.Visual != '#' && gardenPlot.Distance <= 64 && gardenPlot.Distance % 2 == 0)
+            if (gardenPlot.Visual != '#' && gardenPlot.Distance <= steps && gardenPlot.Distance % 2 == 0)
             {
                 countPlots++;
             }
--- src/AdventOfCode.Process/Day22.cs
+++ src/AdventOfCode.Process/Day22.cs
@@ -4,11 +4,210 @@ public class Day22 : IDay
 {
     public string PartA(string[] input)
     {
-        return "Not finished!";
+        List<Brick> bricks = GenerateBricks(input);
+        bricks.Sort();
+        MoveBricks(bricks);
+
+        return CountBricksDisintetrated(bricks).ToString();
     }
 
     public string PartB(string[] input)
     {
-        return "Not finished!";
+        List<Brick> bricks = GenerateBricks(input);
+        bricks.Sort();
+        MoveBricks(bricks);
+
+        return CountBricksFall(bricks).ToString();
+    }
+
+    private static List<Brick> GenerateBricks(string[] input)
+    {
+        List<Brick> bricks = new();
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            string[] c = input[i].Split(',', '~'); // coordinates
+            bricks.Add(new Brick(input[i], int.Parse(c[0]), int.Parse(c[3]), int.Parse(c[1]), int.Parse(c[4]), int.Parse(c[2]), int.Parse(c[5])));
+
+        }
+        return bricks;
+    }
+
+    private static void MoveBricks(List<Brick> bricks)
+    {
+        List<Brick> movedBricks = new();
+
+        foreach (Brick brick in bricks)
+        {
+            bool brickIsBlocked = false;
+            while (!brickIsBlocked)
+            {
+                if (IsBrickBelow(brick, movedBricks))
+                {
+                    movedBricks.Add(brick);
+                    brickIsBlocked = true;
+                }
+                else
+                {
+                    brick.ZStart--;
+                    brick.ZEnd--;
+                }
+            }
+        }
+    }
+
+    private static bool IsBrickBelow(Brick brick, List<Brick> movedBricks)
+    {
+        if (brick.ZStart == 1)
+        {
+            return true;
+        }
+        bool isBrickBelow = false;
+        foreach (Brick movedBrick in movedBricks)
+        {
+            if (movedBrick.IsBelow(brick))
+            {
+                isBrickBelow = true;
+
+            }
+        }
+        return isBrickBelow;
+    }
+
+    private static int CountBricksDisintetrated(List<Brick> bricks)
+    {
+        int countBricks = 0;
+        foreach (Brick brick in bricks)
+        {
+            if (brick.Support.Count == 0)
+            {
+                countBricks++;
+                continue;
+            }
+            else
+            {
+                bool brickCanDisintegrates = true;
+                foreach (Brick support in brick.Support)
+                {
+                    if (support.SupportedBy.Count < 2)
+                    {
+                        brickCanDisintegrates = false;
+                    }
+                }
+                if (brickCanDisintegrates) countBricks++;
+            }
+        }
+        return countBricks;
+    }
+
+    private static int CountBricksFall(List<Brick> bricks)
+    {
+        int countBricks = 0;
+
+        foreach (Brick brick in bricks)
+        {
+            HashSet<Brick> fallenBricks = new()
+            {
+                brick
+            };
+
+            PriorityQueue<Brick, int> pq = new();
+            foreach (Brick supportedBrick in brick.Support)
+            {
+                pq.Enqueue(supportedBrick, supportedBrick.ZStart);
+            }
+
+            while (pq.Count != 0)
+            {
+
+                Brick currentBrick = pq.Dequeue();
+
+                if (!HasSupport(currentBrick, fallenBricks))
+                {
+                    fallenBricks.Add(currentBrick);
+                    foreach (Brick supportedBrick in currentBrick.Support)
+                    {
+                        pq.Enqueue(supportedBrick, supportedBrick.ZStart);
+                    }
+                }
+            }
+
+            countBricks += fallenBricks.Count - 1;
+        }
+
+        return countBricks;
+    }
+
+    private static bool HasSupport(Brick currentBrick, HashSet<Brick> fallenBricks)
+    {
+        foreach (Brick supportedBrick in currentBrick.SupportedBy)
+        {
+            if (!fallenBricks.Contains(supportedBrick))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private class Brick : IComparable<Brick>
+    {
+        public string Data { get; private set; }
+        public int XStart { get; private set; }
+        public int XEnd { get; private set; }
+        public int YStart { get; private set; }
+        public int YEnd { get; private set; }
+        public int ZStart { get; set; }
+        public int ZEnd { get; set; }
+        public List<Brick> SupportedBy { get; set; }
+        public List<Brick> Support { get; set; }
+
+        public Brick(string data, int xStart, int xEnd, int yStart, int yEnd, int zStart, int zEnd)
+        {
+            Data = data;
+            XStart = xStart;
+            XEnd = xEnd;
+            YStart = yStart;
+            YEnd = yEnd;
+            ZStart = zStart;
+            ZEnd = zEnd;
+            SupportedBy = new();
+            Support = new();
+        }
+        public int CompareTo(Brick? other)
+        {
+            if (other == null)
+            {
+                return 0;
+            }
+
+            return ZStart.CompareTo(other.ZStart);
+        }
+        public bool IsBelow(Brick brick)
+        {
+            if (brick.ZStart - 1 == ZEnd)
+            {
+                for (int x = brick.XStart; x <= brick.XEnd; x++)
+                {
+                    for (int y = brick.YStart; y <= brick.YEnd; y++)
+                    {
+                        if (x >= XStart && x <= XEnd && y >= YStart && y <= YEnd)
+                        {
+                            Support.Add(brick);
+                            brick.SupportedBy.Add(this);
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+    }
+
+    private static void PrintBricks(List<Brick> bricks, char direction)
+    {
+
+
+
     }
 }
